e4ad2bfc6c01a7b89bd3ed00e572c3acafd6c5dc
irina <irinkagoms2001@gmail.com>
2026-05-09 17:41:18 +0300

    проверка продолжить

--- src/tasks/task002/Task002.java
+++ src/tasks/task002/Task002.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 
 public class Task002 {
     public static void main(String[] args) {
-        System.out.println("Введите число");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if (a > 20) {
-            System.out.println("Дофига хочешь");
-        } else {
-            for (int i = 0; i <= a; i++) {
-                System.out.println(i);
+        while (true) {
+            System.out.println("Введите число");
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            if (a > 20) {
+                System.out.println("Дофига хочешь");
+            } else {
+                for (int i = 0; i <= a; i++) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Продолжить?");
+            String answer = sc.next();
+            if (answer.equalsIgnoreCase("нет")) {
+                break;
             }
         }
     }
